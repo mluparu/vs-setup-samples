a1a955170a06bfcc4d6190b9d75a280125611bf7
Adam Welch <adwelch@microsoft.com>
2017-02-23 15:20:28 -0800

    Refining sample to only have C++ and find VC++ toolset instances.
    Refining sample to only have C++ and find VC++ toolset instances.

--- Setup.Configuration.VC.NoDeps/Setup.Configuration.h
+++ Setup.Configuration.VC.NoDeps/Setup.Configuration.h
@@ -1,5 +1,6 @@
-// <copyright file="Setup.Configuration.h" company="Microsoft Corporation">
+// <copyright file="Program.cpp" company="Microsoft Corporation">
 // Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt in the project root for license information.
 // </copyright>
 
 #pragma once
